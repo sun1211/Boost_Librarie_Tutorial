aaa6fc307868b0b8d3f6d8ff33d120a111ac622d
sun1211 <ngoctam121191@yahoo.com>
2021-07-21 22:08:45 +0700

    add note for chapter 08, recipe 01, main.cpp

--- chapter_08/recipe_01/main.cpp
+++ chapter_08/recipe_01/main.cpp
@@ -15,6 +15,7 @@ struct variant;
 
 
 #include <boost/mpl/vector.hpp>
+//let's pack all the types in one of the Boost.MPL type's container
 template <
     class T0, class T1, class T2, class T3, class T4,
     class T5, class T6, class T7, class T8, class T9
@@ -23,6 +24,7 @@ struct variant {
     typedef boost::mpl::vector<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> types;
 };
 
+//Let's make our example less abstract and see how it works if we specify types
 #include <string>
 struct declared{ unsigned char data[4096]; };
 struct non_declared;
@@ -36,11 +38,12 @@ typedef variant<
     std::string
 >::types types;
 
+//We may check everything at compile time. Let's assert that types is not empty
 #include <boost/static_assert.hpp>
 #include <boost/mpl/empty.hpp>
 BOOST_STATIC_ASSERT((!boost::mpl::empty<types>::value));
 
-
+// the non_declared types is still at index 4 position
 #include <boost/mpl/at.hpp>
 #include <boost/type_traits/is_same.hpp>
 BOOST_STATIC_ASSERT((boost::is_same<
@@ -48,13 +51,14 @@ BOOST_STATIC_ASSERT((boost::is_same<
     boost::mpl::at_c<types, 4>::type
 >::value));
 
-
+//the last type is still std::string
 #include <boost/mpl/back.hpp>
 BOOST_STATIC_ASSERT((boost::is_same<
     boost::mpl::back<types>::type,
     std::string
 >::value));
 
+//We may carry out some transformations. Let's start with removing constant and volatile qualifiers
 #include <boost/mpl/transform.hpp>
 #include <boost/type_traits/remove_cv.hpp>
 typedef boost::mpl::transform<
@@ -62,12 +66,14 @@ typedef boost::mpl::transform<
     boost::remove_cv<boost::mpl::_1> 
 >::type noncv_types;
 
+//we can remove the duplicate types
 #include <boost/mpl/unique.hpp>
 typedef boost::mpl::unique<
     noncv_types, 
     boost::is_same<boost::mpl::_1, boost::mpl::_2>
 >::type unique_types;
 
+//check that the vector contains only 5 types
 #include <boost/mpl/size.hpp>
 BOOST_STATIC_ASSERT((boost::mpl::size<unique_types>::value == 5));
 
@@ -75,12 +81,14 @@ BOOST_STATIC_ASSERT((boost::mpl::size<unique_types>::value == 5));
 // use of undefined type 'non_declared'
 struct non_declared{};
 
+//compute sizes of each element
 #include <boost/mpl/sizeof.hpp>
 typedef boost::mpl::transform<
     unique_types, 
     boost::mpl::sizeof_<boost::mpl::_1>
 >::type sizes_types;
 
+//get the maximum size from the sizes_type type
 #include <boost/mpl/max_element.hpp>
 typedef boost::mpl::max_element<sizes_types>::type max_size_type;
 
